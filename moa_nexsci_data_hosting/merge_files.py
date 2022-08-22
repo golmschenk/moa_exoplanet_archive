cedd81eccfc8112632aa2a1c9004bb9b850970bd
Greg Olmschenk <greg@olmschenk.com>
2022-08-22 10:48:23 -0400

    Set the corrected flux to NA if the corrected flux was not actually processed

--- moa_nexsci_data_hosting/merge_files.py
+++ moa_nexsci_data_hosting/merge_files.py
@@ -3,6 +3,7 @@ import multiprocessing
 from functools import partial
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 from tabulate import tabulate
 
@@ -31,6 +32,8 @@ def merge_split_version_files_into_single_file(dot_phot_dot_all_path: Path, dest
     unordered_merged_data_frame = pd.merge(dot_phot_dot_all_data_frame,
                                            dot_phot_dot_cor_data_frame[columns_to_merge_in], on=ColumnName.HJD,
                                            how='outer')
+    if unordered_merged_data_frame[ColumnName.AIRMASS_1].isna().all():
+        unordered_merged_data_frame[ColumnName.COR_FLUX] = np.nan
     merged_data_frame = unordered_merged_data_frame[merged_column_names]
     table_string = tabulate(merged_data_frame, headers=merged_column_names, tablefmt='plain', missingval='na',
                             floatfmt=tuple(merged_column_formats.values()), showindex=False, numalign='right')
@@ -49,15 +52,16 @@ def merge_three_version(phot_all_path, three_version_directory, merged_version_d
 
 
 def convert_directory_to_merged_version(three_version_directory: Path, merged_version_directory: Path):
-    pool = multiprocessing.Pool(processes=20)
     merge_three_version_with_roots = partial(merge_three_version,
                                              three_version_directory=three_version_directory,
                                              merged_version_directory=merged_version_directory)
     paths = three_version_directory.glob('**/*.phot.all.gz')
-    pool.map(merge_three_version_with_roots, paths)
+    with multiprocessing.get_context('spawn').Pool(processes=20, maxtasksperchild=200) as pool:
+        for _ in pool.imap_unordered(merge_three_version_with_roots, paths):
+            pass
 
 
 if __name__ == '__main__':
     three_version_directory_ = Path('MOA2dia')
-    merged_version_directory_ = Path('merged_MOA2dia')
+    merged_version_directory_ = Path('merged_MOA2dia_cor_flux_na')
     convert_directory_to_merged_version(three_version_directory_, merged_version_directory_)
