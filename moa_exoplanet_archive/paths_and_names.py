0747bf9ca55433ebc713f624f653599355c500a2
golmschenk <greg@olmschenk.com>
2023-04-24 14:34:56 -0400

    Add paths and names file

---
+++ moa_exoplanet_archive/paths_and_names.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+osaka_entry_in_ssh_config = 'osaka'
+osaka_data_directories = [
+    Path("/moao38_3/sumi/MOA2dia"), Path("/moao36_3/sumi/MOA2dia"), Path("/moao16_3/sumi/MOA2dia"),
+    Path("/moao29_3/sumi/MOA2dia"), Path("/moao17_3/sumi/MOA2dia"), Path("/moao18_3/sumi/MOA2dia"),
+    Path("/moao39_3/sumi/MOA2dia"), Path("/moao37_3/sumi/MOA2dia"), Path("/moao33_3/sumi/MOA2dia"),
+    Path("/moao34_3/sumi/MOA2dia"), Path("/moao25_3/sumi/MOA2dia"), Path("/moao10_3/sumi/MOA2dia"),
+    Path("/moao15_3/sumi/MOA2dia"), Path("/moao35_3/sumi/MOA2dia"), Path("/moao21_3/sumi/MOA2dia"),
+    Path("/moao22_3/sumi/MOA2dia"), Path("/moao19_3/sumi/MOA2dia"), Path("/moao20_3/sumi/MOA2dia"),
+    Path("/moao12_3/sumi/MOA2dia"), Path("/moao8_3/sumi/MOA2dia"), Path("/moao30_3/sumi/MOA2dia"),
+    Path("/moao24_3/sumi/MOA2dia"), Path("/moao32_3/sumi/MOA2dia"), Path("/moao7_3/sumi/MOA2dia"),
+    Path("/moao11_3/sumi/MOA2dia"), Path("/moao2_3/sumi/MOA2dia"), Path("/moao4_3/sumi/MOA2dia"),
+    Path("/moao6_3/sumi/MOA2dia")
+]
+local_directory = 'moa_exoplanet_archive_data'
+osaka_cut0_object_subdirectory = 'cut0.chi2NoCJD8dupSR14CSm2p8PSsdf21'
+local_cut0_object_directory = 'cut0_object_files'
