88897ee6f52ac027ffc52b7841c3d37f24ac0f39
golmschenk <greg@olmschenk.com>
2023-09-12 18:33:38 -0400

    Progress bar

--- moa_exoplanet_archive/light_curve_format_post_processing.py
+++ moa_exoplanet_archive/light_curve_format_post_processing.py
@@ -1,19 +1,24 @@
 import gzip
+from multiprocessing import Pool
 from pathlib import Path
+
+import tqdm as tqdm
 
 
 def post_process():
     light_curve_root_directory = Path('/local/data/emu/share/exoplanet_archive_moa_dataset/exoplanet_archive_moa_dataset/lcurve/lcurve')
-    light_curve_path_glob = light_curve_root_directory.glob(r'**/*.ipac.gz')
-    for light_curve_index, light_curve_path in enumerate(light_curve_path_glob):
-        print(f'{light_curve_index}: {light_curve_path}')
-        with gzip.open(light_curve_path, 'rt') as unprocessed_file:
-            unprocessed_light_curve_string = unprocessed_file.read()
+    light_curve_path_list = list(light_curve_root_directory.glob(r'**/*.ipac.gz'))
+    with Pool(20) as pool:
+        for _ in tqdm.tqdm(pool.imap_unordered(replace_null_string, light_curve_path_list), total=len(light_curve_path_list)):
+            pass
 
-        processed_light_curve_string = unprocessed_light_curve_string.replace('null|', ' nan|')
 
-        with gzip.open(light_curve_path, 'wt') as processed_file:
-            processed_file.write(processed_light_curve_string)
+def replace_null_string(light_curve_path):
+    with gzip.open(light_curve_path, 'rt') as unprocessed_file:
+        unprocessed_light_curve_string = unprocessed_file.read()
+    processed_light_curve_string = unprocessed_light_curve_string.replace('null|', ' nan|')
+    with gzip.open(light_curve_path, 'wt') as processed_file:
+        processed_file.write(processed_light_curve_string)
 
 
 if __name__ == '__main__':
